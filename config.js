398d59668abbea2294feedf0dadec5c57de8735d
vict33m <144964153+vict33m@users.noreply.github.com>
2023-09-13 20:08:47 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://awesome.joescriptf.workers.dev",
+  VITE_TMDB_READ_API_KEY: "4111c7c58bf4643ee076f489d65e0b8b"
+};
